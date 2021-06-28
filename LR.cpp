2fc375e57ff16f20e5291ea69b7e4b05315f4929
AnastasiaTsvetkova <86355641+AnastasiaTsvetkova@users.noreply.github.com>
2021-06-28 14:53:40 +0300

    Add files via upload

---
+++ LR.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int max(int x, int y, int z)
+{
+    int m = 0;
+    if ((x > y) && (x > z)) m = x;
+    if ((y > x) && (y > z)) m = y; 
+    if ((z > x) && (c > y)) m = z; 
+    return m;
+}
+int main()
+{
+    setlocale (LC_ALL, "Russian"); 
+    int x, y, z;
+    cout << "Введите числа:" << endl;
+    cin >>x >> y>> z;
+    cout << "Максимальное число=" << max(x, y, z)<<endl; //вызов функции
+    system("pause");
+    return 0;
+
+}
